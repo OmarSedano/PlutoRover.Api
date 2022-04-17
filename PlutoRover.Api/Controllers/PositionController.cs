5038eccde75554d08761213a3e9e76658ec18017
omar.sedano <omar.sedano@CESNB1113>
2022-04-18 00:43:55 +0200

    added Readme

--- PlutoRover.Api/Controllers/PositionController.cs
+++ PlutoRover.Api/Controllers/PositionController.cs
@@ -15,6 +15,21 @@ namespace PlutoRover.Api.Controllers
             _positionService = positionService ?? throw new ArgumentNullException(nameof(positionService));
         }
 
+        /// <summary>
+        /// Set the position of the Rover through commands
+        /// </summary>
+        /// <returns>Success/Fail</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Position
+        ///     {
+        ///        "commands": "FFRFLB"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">The position was successfully setted</response>
+        /// <response code="400">The commands are invalid.</response>
         [HttpPost]
         public ActionResult Post(PositionModel positionModel)
         {
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# PlutoRover.Api
+
+Solution that exposes an API to communicate with Rover.
+This is a ASP.NET Core API with framework .NET 6.
+To make it work we have to run the project PlutoRover.Api.
+Once is started the API it will open in the browser a window with the path {localhost}/swagger/index.html. where we could find the Swagger of the API.
+
